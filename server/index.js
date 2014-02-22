78ad5cc311e77e7d90a377f7b085341e8f317e7d
Sven Lito <me@svenlito.com>
2014-02-22 14:24:12 +0100

    [minor] chuck out routes

--- server/index.js
+++ server/index.js
@@ -1,16 +1,5 @@
 var Hapi = require('hapi');
-var path = require('path');
 
 var app = new Hapi.Server('localhost', 4444);
-
-app.route([
-  {
-    method: 'GET',
-    path: '/_api/_plugins',
-    handler: function (req, reply) {
-      reply.file(path.resolve('./data/plugins.json'));
-    }
-  }
-]);
 
 module.exports = app;
