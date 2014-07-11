e7ed334fbb7bb0787bd40c7b285ec9560da3e0df
Gregor Martynus <gregor@martynus.net>
2014-07-11 02:28:00 +0200

    feat: inject hoodieAdmin & require into plugin iframes
    * * *
    
    This commit was sponsored by The Hoodie Firm.
    You can hire The Hoodie Firm:
    http://go.hood.ie/thehoodiefirm

--- src/script/components/pocket/entities/adminEntity.js
+++ src/script/components/pocket/entities/adminEntity.js
@@ -4,9 +4,6 @@ var app = require('../../../helpers/namespace');
 var HoodieAdmin = require('hoodie.admin');
 var admin = new HoodieAdmin();
 
-// shortcut for plugin: top.hoodieAdmin...
-window.hoodieAdmin = admin;
-
 app.reqres.setHandler('admin', function () {
   return admin;
 });
