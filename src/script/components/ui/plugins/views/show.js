864189c7735345f258a29417fd6e529c2d3118bb
Gregor Martynus <gregor@martynus.net>
2014-07-11 02:28:00 +0200

    feat: inject hoodieAdmin & require into plugin iframes
    * * *
    
    This commit was sponsored by The Hoodie Firm.
    You can hire The Hoodie Firm:
    http://go.hood.ie/thehoodiefirm

--- src/script/components/ui/plugins/views/show.js
+++ src/script/components/ui/plugins/views/show.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var Marionette = require('backbone.marionette');
+var _ = require('underscore');
 
 require('../../../../helpers/handlebars');
 
@@ -11,6 +12,17 @@ var View = Marionette.ItemView.extend({
   template: tmpl,
   initialize: function (options) {
     this.options = options || {};
+    _.bindAll(this, 'injectHoodieAdmin');
+  },
+  ui: {
+    iframe: 'iframe'
+  },
+  onRender: function() {
+    this.ui.iframe.on('load', this.injectHoodieAdmin);
+  },
+  injectHoodieAdmin: function () {
+    this.ui.iframe[0].contentWindow.require = require;
+    this.ui.iframe[0].contentWindow.hoodieAdmin = app.request('admin');
   }
 });
 
