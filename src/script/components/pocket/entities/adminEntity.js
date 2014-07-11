d0b753a4f4bd8239c7fa71e0a6204a7d5c234c23
Gregor Martynus <gregor@martynus.net>
2014-07-11 13:05:31 +0200

    fix: set window.hoodieAdmin for plugins
    * * *
    
    This commit was sponsored by The Hoodie Firm.
    You can hire The Hoodie Firm:
    http://go.hood.ie/thehoodiefirm

--- src/script/components/pocket/entities/adminEntity.js
+++ src/script/components/pocket/entities/adminEntity.js
@@ -4,6 +4,8 @@ var app = require('../../../helpers/namespace');
 var HoodieAdmin = require('hoodie.admin');
 var admin = new HoodieAdmin();
 
+global.hoodieAdmin = admin;
+
 app.reqres.setHandler('admin', function () {
   return admin;
 });
