4687077f0254ff0fbe7a9693854180c163be47da
Gregor Martynus <gregor@martynus.net>
2014-07-13 18:47:53 +0200

    chore: removed unused plugin routes and code
    * * *
    
    This commit was sponsored by The Hoodie Firm.
    You can hire The Hoodie Firm:
    http://go.hood.ie/thehoodiefirm

--- src/script/components/ui/plugins/controllers/index.js
+++ src/script/components/ui/plugins/controllers/index.js
@@ -3,37 +3,15 @@
 var app = require('../../../../helpers/namespace');
 var Marionette = require('backbone.marionette');
 
-var ListView = require('../views/list');
 var ShowView = require('../views/show');
-var EditView = require('../views/edit');
-
 var Controller = Marionette.Controller.extend({
 
   initialize: function (options) {
     this.options = options || {};
   },
 
-  list: function (opts) {
-    var view = new ListView({
-      collection: opts.collection,
-      ns: opts.ns
-    });
-
-    this.showView(view);
-  },
-
   show: function (opts) {
     var view = new ShowView({
-      collection: opts.collection,
-      model: opts.model,
-      ns: opts.ns
-    });
-
-    this.showView(view);
-  },
-
-  edit: function (opts) {
-    var view = new EditView({
       collection: opts.collection,
       model: opts.model,
       ns: opts.ns
