0cda86b5c18235fa322ba2f3fb991da6163c87ee
Alex Feyerke <espy@users.noreply.github.com>
2015-07-16 09:27:21 +0200

    WIP, still trying to adapt hoodie API to ember style

--- app/routes/plugins.js
+++ app/routes/plugins.js
@@ -1,9 +1,13 @@
+import Ember from 'ember';
 import AuthenticatedRoute from '../routes/authenticated';
 
 export default AuthenticatedRoute.extend({
   model: function() {
     var plugins = this.store.findAll('plugin');
-    console.log('plugins: ',plugins);
     return plugins;
+  },
+  afterModel: function (plugins) {
+    var m = Ember.inspect(plugins);
+    console.log('m: ',m);
   }
 });
--- app/serializers/plugin.js
+++ app/serializers/plugin.js
@@ -1,16 +1,25 @@
 import DS from 'ember-data';
 
 export default DS.RESTSerializer.extend({
+  primaryKey: 'name'/*,
+  extractSingle: function(store, typeClass, payload, id) {
+    console.log('id: ',id);
+    console.log('payload: ',payload);
+    console.log('typeClass: ',typeClass);
+    console.log('store: ',store);
+    var comments = payload._embedded.comment;
+    delete payload._embedded;
+
+    payload = { comments: comments, post: payload };
+    return this._super(store, typeClass, payload, id);
+  },
   extractArray: function (store, primaryType, payload) {
       var primaryTypeName = primaryType.modelName;
-      console.log('primaryTypeName: ',primaryTypeName);
       var data = {};
-      data['plugins'] = payload;
+      data[primaryTypeName.pluralize()] = payload;
       console.log(JSON.stringify(data));
       payload = data;
-      console.log('payload: ',payload);
 
       return this._super.apply(store, primaryType, payload);
-  },
-  primaryKey: 'name'
+  }*/
 });
