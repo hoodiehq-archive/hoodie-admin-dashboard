7b0100c6c98ca7d3390b2fd5b44a72db3523f1e6
Alex Feyerke <espy@users.noreply.github.com>
2015-07-30 15:52:11 +0200

    Removed Futon link in users table

--- app/helpers/link-to-futon-user.js
+++ app/helpers/link-to-futon-user.js
@@ -1,7 +1,12 @@
 import Ember from 'ember';
 
 export function linkToFutonUser(userName) {
-  return window.location.origin + '/_api/_utils/document.html?_users/org.couchdb.user:' + userName;
+  // FIX: linking to Futon doesn't work when using `ember serve --proxy http://localHoodieEndpoint`
+
+  // FIX: linking to Futon also doesn't work inside the deployed app, since
+  // http://appEndpoint/_api/_utils !== http://CouchDBEndpoint/_utils
+
+  return window.location.origin + '/_utils/document.html?_users/org.couchdb.user%3Auser%2F' + userName;
 }
 
 export default Ember.Helper.helper(linkToFutonUser);
