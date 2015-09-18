6a304676f96bb488b2f29f83fbc700edd04b1806
Alex Feyerke <espy@users.noreply.github.com>
2015-09-18 18:39:10 +0200

    feat(libraries,build): guide and uikit are part of panel, and look the same as they used to
    BREAKING CHANGE: The old paths to the UIKit assets will no longer work, /_api/_plugins/_assets/styles/admin-dashboard-uikit.css etc. Instead, the assets are served by the Dashboard, at /hoodie/admin/assets.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,5 +1,9 @@
 /* global require, module */
+
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+var concat = require('broccoli-concat');
+var uglifyJS = require('broccoli-uglify-js');
+var Funnel = require('broccoli-funnel');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
@@ -31,5 +35,67 @@ module.exports = function(defaults) {
   // The JS files get appended to www/assets/vendor.js, which already includes ember
   app.import('bower_components/bootstrap/js/modal.js');
 
-  return app.toTree();
+  // Copy the UIKit guide files over to www
+  var guide = new Funnel('guide', {
+    destDir: 'guide'
+  });
+
+  // Copy the Select2 image files over to www
+  var select2 = new Funnel('bower_components/select2', {
+    include: ['*.png', '*.gif'],
+    destDir: 'assets'
+  });
+
+  // Copy the dropzone image files over to www
+  var dropzone = new Funnel('bower_components/dropzone/downloads', {
+    include: ['*.png'],
+    destDir: 'assets'
+  });
+
+  // Copy prism over to assets
+  var prism = new Funnel('bower_components/prism', {
+    include: [
+      'prism.js',
+      'themes/prism.css'
+    ],
+    destDir: 'assets',
+    getDestinationPath: function(relativePath) {
+      if (relativePath === 'themes/prism.css') {
+        return 'prism.css';
+      }
+      return relativePath;
+    }
+  });
+
+  // Copy icheck sprites over to assets
+  var icheck = new Funnel('bower_components/jquery-icheck', {
+    include: [
+      'skins/flat/green*.png'
+    ],
+    destDir: 'assets',
+    getDestinationPath: function(relativePath) {
+      return relativePath.split('/').reverse()[0];
+    }
+  });
+
+  // Everything that needs to go into UIKit.js is
+  // concatenated and minified here, everytime $ember build is run
+  // TODO: should maybe include bootstrap js files
+  // TODO: should maybe include prism.js
+  var UIKitJS = concat('.', {
+    inputFiles: [
+      'bower_components/jquery/dist/jquery.js',
+      'bower_components/jquery-icheck/icheck.js',
+      'bower_components/dropzone/downloads/dropzone.js',
+      'bower_components/select2/select2.js',
+      'vendor/uikit-init.js'
+    ],
+    outputFile: '/assets/uikit.js'
+  });
+
+  UIKitJS = uglifyJS(UIKitJS, {
+    compress: true
+  });
+
+  return app.toTree([UIKitJS, guide, prism, icheck, select2, dropzone]);
 };
